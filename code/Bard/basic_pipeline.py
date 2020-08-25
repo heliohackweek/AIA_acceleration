f202d3a738faefcaf1c2aeb89b0e0c1cd7c6b521
Bard <>
2020-08-25 17:38:09 -0400

    Fixed small bug in basic pipeline (credit: Peter Schuck); and added capability to save reduced data in new folder

--- code/Bard/basic_pipeline.py
+++ code/Bard/basic_pipeline.py
@@ -31,7 +31,9 @@ def scrape_AIA_files(path):
 
 # converts lvl1 files in lvl1_path/lvl1_fname_list to lvl1.5
 # puts lvl1.5 files in same directory as lvl1 (but named aia_lev1.5)
-def gen_lvl_1p5(lvl1_fname_list,lvl1_path):
+def gen_lvl_1p5(lvl1_fname_list,lvl1_path, lvl5_path=None):
+    if lvl5_path is None:
+        lvl5_path = lvl1_path
     for af in lvl1_fname_list:
         m = sunpy.map.Map(lvl1_path+'/'+af)
         m_up = update_pointing(m)
@@ -39,14 +41,17 @@ def gen_lvl_1p5(lvl1_fname_list,lvl1_path):
         m_norm = sunpy.map.Map(m_reg.data/m_reg.exposure_time.to(u.s).value,
                                m_reg.meta)
         new_name = af.replace('lev1', 'lev1.5')
-        m_norm.save(lvl1_path+'/'+new_name)
+        m_norm.save(lvl5_path+'/'+new_name)
         print('to lvl 1.5: {} at {}'.format(str(m_norm.wavelength),str(m_norm.date)))
     return
 
 # deconvolution pipeline example; only does one wavelength at a time
 #  (since deconvolution psf is unique for each wavelength)
 # returns list of skipped filenames
-def gen_deconv(lvl1_fname_list, lvl1_path):
+def gen_deconv(lvl1_fname_list, lvl1_path, lvldec_path=None):
+    if lvldec_path is None:
+        lvldec_path = lvl1_path
+        
     # do first image/calculate psf
     af = lvl1_fname_list[0]
     m = sunpy.map.Map(lvl1_path+"/"+af)
@@ -58,7 +63,7 @@ def gen_deconv(lvl1_fname_list, lvl1_path):
     # finish for first image
     m_decov = aiapy.psf.deconvolve(m,psf=psf)
     new_name = af.replace('lev1', 'levDecv')
-    m_decov.save(lvl1_path+'/'+new_name)
+    m_decov.save(lvldec_path+'/'+new_name)
     print('deconvolved: {} at {}'.format(str(m_decov.wavelength),str(m_decov.date)))
 
     skipped_files = []
@@ -71,7 +76,7 @@ def gen_deconv(lvl1_fname_list, lvl1_path):
             continue
         m_decov = aiapy.psf.deconvolve(m,psf=psf)
         new_name = af.replace('lev1', 'levDecv')
-        m_decov.save(lvl1_path+'/'+new_name)
+        m_decov.save(lvldec_path+'/'+new_name)
         print('deconvolved: {} at {}'.format(str(m_decov.wavelength),str(m_decov.date)))
     
     return skipped_files
@@ -79,21 +84,25 @@ def gen_deconv(lvl1_fname_list, lvl1_path):
 if __name__ == '__main__':
 
     lvl1_path = 'AIA_data/171A/'
+    lvl1p5_path = 'AIA_data/171A_lvl1.5/'
+    lvldec_path = 'AIA_data/171A_dec/'
     aia_files = scrape_AIA_files(lvl1_path)
 
     #for example, only do first five files
     aia_files_samp = aia_files[:5]
 
     #outputs: aia_lev1.5_*_lev1.5.fits 
-    gen_lvl_1p5(aia_files,lvl1_path) 
+    gen_lvl_1p5(aia_files_samp,lvl1_path, lvl1p5_path) 
 
+    print("end 1->1.5")
     # WARNING: following takes many minutes to calculate psf
     #  and then a few minutes to deconvolve each image
     #  (expected run time: ~30-40 min if no GPU/multithreading)
     #  (a minute or two if GPU? <-- needs verification)
     #outputs: aia_levDecv_*_levDecv.fits
-    """
+    
     while aia_files_samp:
-        aia_files_samp = gen_deconv(aia_files_samp, lvl1_path)
-    """
-
+        aia_files_samp = gen_deconv(aia_files_samp, lvl1_path,lvldec_path)
+        print("end of one loop")
+    
+    
